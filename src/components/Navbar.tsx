6bde486ab95ba5791fb6eec6e25cedf1274276b6
Ehoneah-Frank <ehoneahfrank2017@gmail.com>
2024-12-30 12:28:07 +0000

    favicon done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Menu, X, Phone, Mail } from "lucide-react";
-import FaviconLogo from "../images/faviconlogo.png";
+import FaviconLogo from "../images/Faviconlogo.png";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
